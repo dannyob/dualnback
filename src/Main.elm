2ef871cf8006771e2d8336e80fb39c73c634c260
Danny O'Brien <Danny O'Brien danny@spesh.com>
2016-06-25 22:35:55 -0700

    Setup barebones Elm app

--- src/Main.elm
+++ src/Main.elm
@@ -1,63 +1,28 @@
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html, table, tr, td, text, div)
+import Html.Attributes exposing (class)
 import Html.App as Html
-import Html.Events exposing ( onClick )
 
--- component import example
-import Components.Hello exposing ( hello )
+-- Model
 
+type alias Model = { note: String  }
 
--- APP
-main : Program Never
-main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+type Msg = Reset
 
-
--- MODEL
-type alias Model = Int
-
-model : number
-model = 0
-
-
--- UPDATE
-type Msg = NoOp | Increment
-
-update : Msg -> Model -> Model
+update: Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        Reset -> { note = "hello" }
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-view : Model -> Html Msg
-view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
+view :Model -> Html Msg
+view model = table [] [ 
+    tr [ class "row1" ] [ td [ class "col1" ] [ text "x" ] , td [ class "col2"] [ text "y" ] , td [ class "col3" ] [ text "z" ] ],
+    tr [ class "row2" ] [ td [ class "col1" ] [ text "x" ] , td [ class "col2"] [ text "y" ] , td [ class "col3" ] [ text "z" ] ],
+    tr [ class "row3" ] [ td [ class "col1" ] [ text "x" ] , td [ class "col2"] [ text "y" ] , td [ class "col3" ] [ text "z" ] ]
     ]
-  ]
 
 
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+model : Model 
+model = { note = "starting" }
+
+main = Html.beginnerProgram { model = model, view = view, update = update }
