e6dd624ab58e23b1c44ef254cee6167adbd4c44b
Danny O'Brien <danny@spesh.com>
2016-08-12 01:03:53 -0700

    Markdownify instructions

--- src/Instructions.elm
+++ src/Instructions.elm
@@ -1,35 +1,54 @@
 module Instructions exposing (instructionsHtml)
 
 import Markdown
-import Html
-instructions = """
-Welcome to Dual N Back, the mind game that ACTUAL scientists 
-believe might improve your IQ. 
+import Html exposing (Html)
 
-(WARNING: see giant footnote in tiny font.)
 
-How to Play
+instructions : String
+instructions =
+    """
+Welcome to *Dual N Back*, the mind game that **ACTUAL** scientists
+believe may improve your IQ.
 
-See this grid?
+<small>(WARNING: see </small>giant<small> footnote in <small>tiny</small> font.)</small>
 
-Every two seconds, a new spot will light up on it, and 
-a voice will say a letter.
+# How to Play
 
-Your job is to notice when the latest spot is in same place as the spot TWO steps 
-back, OR when the voice says the same letter as two steps back.
+## Behold The Grid!
 
-If the spots match, hit the 'S' key. If the letters match, hit the 'L' key.
+[INSERT ANIMATED GRID HERE]
+
+Every two seconds, a new spot will light up on this grid, and a voice will tell
+you letter.
+
+<div class="bubble">M!</div>
+
+Your job is to notice when the current spot is in same place as the spot TWO
+steps back, OR when the voice says the same letter as two steps back.
+
+[ANIMATED GRID SHOWING WHERE THINGS WERE PREVIOUSLY]
+
+## Keys
+
+If the spots match, hit the <span class="keyboard">S</span> key. If the letters match, hit the <span class="keyboard">L</span> key.
 
 If they both match, hit both keys (in any order).
+
+## Scoring
 
 Every time you make a match, you'll get a point.
 
 Every time you miss a match, you'll lose a point.
+
+## Levelling Up
 
 If you do well enough, we'll start matching spots and letters THREE steps back.
 
 Ready to play?
 
 """
-instructionsHtml = Markdown.toHtml [] instructions
 
+
+instructionsHtml : Html msg
+instructionsHtml =
+    Markdown.toHtml [] instructions
